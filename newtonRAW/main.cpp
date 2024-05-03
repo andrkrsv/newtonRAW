eafd0f708f8ea36985218b6ebd74e5fa87e5919d
Andrii Kurasov <andrkrsv@gmail.com>
2024-05-03 13:37:04 +0300

    newton raffson

--- newtonRAW/main.cpp
+++ newtonRAW/main.cpp
@@ -1,14 +1,50 @@
-//
-//  main.cpp
-//  newtonRAW
-//
-//  Created by Andrii Kurasov on 03.05.2024.
-//
-
 #include <iostream>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+// Система функций
+double f1(double x, double y) {
+    return x * x + y * y - 5;
+}
+
+double f2(double x, double y) {
+    return x - y - 1;
+}
+
+// Производные системы функций
+double f1_x(double x, double y) {
+    return 2 * x;
+}
+
+double f1_y(double x, double y) {
+    return 2 * y;
+}
+
+double f2_x(double x, double y) {
+    return 1;
+}
+
+double f2_y(double x, double y) {
+    return -1;
+}
+
+// Решение СЛАУ методом Ньютона-Рафсона
+void solve_SLAE_NR(double x0, double y0, double eps) {
+    // Итерации метода Ньютона-Рафсона
+    for (int i = 0; i < 100; ++i) {
+        double det = f1_x(x0, y0) * f2_y(x0, y0) - f1_y(x0, y0) * f2_x(x0, y0);
+
+        double delta_x = (f2(x0, y0) * f1_y(x0, y0) - f1(x0, y0) * f2_y(x0, y0)) / det;
+        double delta_y = (f1(x0, y0) * f2_x(x0, y0) - f2(x0, y0) * f1_x(x0, y0)) / det;
+
+        x0 += delta_x;
+        y0 += delta_y;
+
+        if (abs(delta_x) < eps && abs(delta_y) < eps) {
+            break;
+        }
+    }
+
+    cout << "Решение уравнения: x = " << x0 << ", y = " << y0 << endl;
 }
