2234b47c641b5536304881374e4046f3ac099b43
Andrii Kurasov <andrkrsv@gmail.com>
2024-05-03 14:13:25 +0300

    upd

--- newtonRAW/main.cpp
+++ newtonRAW/main.cpp
@@ -1,47 +1,48 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
-using namespace std;
-
-double f1(double x, double y) {
-    return x * x + y * y - 5;
+auto f(const std::vector<double>& x) {
+    return std::vector<double>{
+        std::sin(x[1]) - 2 * x[0] - 1,
+        std::cos(x[0] + 0.5) - x[1] - 2
+    };
 }
 
-double f2(double x, double y) {
-    return x - y - 1;
+auto jacobian(const std::vector<double>& x) {
+    return std::vector<std::vector<double>>{
+        {-std::sin(2*x[0] + 1), 1},
+        {2, std::cos(x[1])}
+    };
 }
 
-double f1_x(double x, double y) {
-    return 2 * x;
+auto newton_raphson(const std::vector<double>& x, double eps) {
+    std::vector<double> result = x;
+    std::vector<double> delta_x(x.size());
+    do {
+        auto fx = f(result);
+        auto J = jacobian(result);
+
+        double det_inv = 1.0 / (J[0][0] * J[1][1] - J[0][1] * J[1][0]);
+        std::vector<std::vector<double>> J_inv{
+            {J[1][1] * det_inv, -J[0][1] * det_inv},
+            {-J[1][0] * det_inv, J[0][0] * det_inv}
+        };
+
+        delta_x[0] = J_inv[0][0] * fx[0] + J_inv[0][1] * fx[1];
+        delta_x[1] = J_inv[1][0] * fx[0] + J_inv[1][1] * fx[1];
+
+        result[0] -= delta_x[0];
+        result[1] -= delta_x[1];
+    } while (std::sqrt(delta_x[0] * delta_x[0] + delta_x[1] * delta_x[1]) < eps);
+    return result;
 }
 
-double f1_y(double x, double y) {
-    return 2 * y;
+int main() {
+    double eps = 1e-4;
+    std::vector<double> x{0.0, 0.0};
+    auto result = newton_raphson(x, eps);
+    std::cout << "Solution: " << "\n" << result[0] << "\n" << result[1] << "\n";
+    return 0;
 }
 
-double f2_x(double x, double y) {
-    return 1;
-}
-
-double f2_y(double x, double y) {
-    return -1;
-}
-
-// Решение СЛАУ методом Ньютона-Рафсона
-void solve_SLAE_NR(double x0, double y0, double eps) {
-    for (int i = 0; i < 100; ++i) {
-        double det = f1_x(x0, y0) * f2_y(x0, y0) - f1_y(x0, y0) * f2_x(x0, y0);
-
-        double delta_x = (f2(x0, y0) * f1_y(x0, y0) - f1(x0, y0) * f2_y(x0, y0)) / det;
-        double delta_y = (f1(x0, y0) * f2_x(x0, y0) - f2(x0, y0) * f1_x(x0, y0)) / det;
-
-        x0 += delta_x;
-        y0 += delta_y;
-
-        if (abs(delta_x) < eps && abs(delta_y) < eps) {
-            break;
-        }
-    }
-
-    cout << "x = " << x0 << ", y = " << y0 << endl;
-}
