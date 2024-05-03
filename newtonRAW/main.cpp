0957f2370ab3fe1449de3e7e9c81e46ac0ff2316
Andrii Kurasov <andrkrsv@gmail.com>
2024-05-03 13:36:46 +0300

    Initial Commit

---
+++ newtonRAW/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  newtonRAW
+//
+//  Created by Andrii Kurasov on 03.05.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
